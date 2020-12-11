668ab63a214f6d1e8bbbc2d894f783cedc1ffaf5
Anders Schneiderman <aschneiderman@users.noreply.github.com>
2020-12-11 14:41:27 -0500

    Monkeyed with the text

--- simple_test.py
+++ simple_test.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 
 print("here we go")
 driver = webdriver.Chrome()
+
+driver.switch_to.new_window('tab')
+
 driver.get("http://www.python.org")
 
 print('and we are done')
