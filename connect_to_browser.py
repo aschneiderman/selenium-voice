3b90ae4d7aa4633fc73054f4b9d3d6332e81bb12
Anders Schneiderman <aschneiderman@users.noreply.github.com>
2021-05-28 12:42:07 -0400

    Cleanup the code

--- connect_to_browser.py
+++ connect_to_browser.py
@@ -1,28 +1,21 @@
-# chrome.exe -remote-debugging-port=9222
+# This script relies on connecting to a version of Google Chrome that is in remote debugging mode.
+# NOTE: this will start up Google, using your current user profile, so all of your cookies & logins willl work
+# You can use whatever port you want; just change the script below
+#
+# To get it up and running, run the following 2 commands from the command prompt (clicck on the search box, type "cmd")
 # cd "C:\Program Files (x86)\Google\Chrome\Application"
 # chrome.exe -remote-debugging-port=9014 --user-data-dir="%LOCALAPPDATA%\Google\Chrome\User Data"
 
 import selenium
 from selenium import webdriver
-
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_experimental_option("debuggerAddress", "localhost:9014")
 driver = webdriver.Chrome(options=chrome_options)
-# driver = webdriver.Chrome()
 
 print "here we go"
-driver.get("https://www.python.org/")
-
-# print "here we go"
 
 driver.get("https://trello.com/b/jbIYuZmS/anders-client-test")
 
-# driver.get("https://www.nytimes.com/")
-# # driver.get("https://www.google.com/")
 
-
- 
-
-# https://stackoverflow.com/questions/51214668/python-selenium-how-to-use-debugger-address-option-in-chrome-driver-for-remote
